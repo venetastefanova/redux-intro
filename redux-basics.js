520f2f3355699f477ccc7f7d475e79d154edc765
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-09-03 18:49:47 +0300

    adding subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -26,10 +26,15 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer); // the store is created by passing a reducer
 console.log(store.getState()); // shows the current state 
 
-
+//======subscription=========
+//subscriptions are triggered when state changes and returns it to us
+store.subscribe(()=>{
+    console.log('[Subscription]', store.getState()); 
+})
 
 //========dispatching an action=======
 store.dispatch({type:'INC_COUNTER'});
 store.dispatch({type:'ADD_COUNTER', value: 10});
 console.log(store.getState());
-//subscription
+
+
